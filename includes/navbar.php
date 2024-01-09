71b8e2fbf00e6f4bf5cc839bbf2ab4376b634dab
vladvld13 <62468852+vladvld13@users.noreply.github.com>
2024-01-09 02:07:19 +0200

    Add files via upload

---
+++ includes/navbar.php
@@ -0,0 +1,34 @@
+<div class="container">
+    <div class="row">
+            <div class="col-md-12">
+            <nav class="navbar navbar-expand-lg navbar-light" style="background-color: lightyellow; position: fixed;left:0;right:0;" >
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php"><img src ='logox.png' id="img"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="oferte_speciale.php">Last Minute</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Contul Meu
+                    </a>
+                    <ul class="dropdown-menu" style="background-color: lightyellow;">
+                        <li><a class="dropdown-item">Deconectare</a></li>
+                        <li><a class="dropdown-item">Sterge Contul</a></li>
+                    </ul>
+
+                </ul>
+                
+                </div>
+            </div>
+            </nav>
+        </div>
+    </div>
+</div>
